52be0d3e3cf2f3b61d78a13713ba5fededfa7d27
= <=>
2022-10-08 16:45:36 +0530

    Setting up routes and controllers structure

--- server.js
+++ server.js
@@ -4,12 +4,15 @@ const dotenv = require('dotenv');
 //load env vars
 dotenv.config();
 
+// Route files
+const bootcamps = require('./routes/bootcamp');
+
 const app = express();
 if (process.env.NODE_ENV === 'development') app.use(express.json());
 
+//Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
 const PORT = process.env.PORT || 5000;
-app.get('/', (req, res) => {
-  res.send('manas');
-});
 
 app.listen(PORT, console.log(`server is running on ${PORT}`));
